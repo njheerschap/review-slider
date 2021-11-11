58d06a4a5a438538bc575b5974d01780ffd57521
njheerschap <78928590+njheerschap@users.noreply.github.com>
2021-11-10 22:20:45 -0500

    Updates

---
+++ precache-manifest.4553c1ef8b2c2707b45829d43a035a5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4ecdfec24217fa1d3a74bc9a158746c",
+    "url": "/review-slider/index.html"
+  },
+  {
+    "revision": "b3f26e0478411b979a42",
+    "url": "/review-slider/static/css/main.7b929783.chunk.css"
+  },
+  {
+    "revision": "e1c4e21724a78a9e8542",
+    "url": "/review-slider/static/js/2.2c3ffba9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/review-slider/static/js/2.2c3ffba9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3f26e0478411b979a42",
+    "url": "/review-slider/static/js/main.5994dcb2.chunk.js"
+  },
+  {
+    "revision": "5dfce26c7b9e33ecdd56",
+    "url": "/review-slider/static/js/runtime-main.3c0fd3a3.js"
+  }
+]);
